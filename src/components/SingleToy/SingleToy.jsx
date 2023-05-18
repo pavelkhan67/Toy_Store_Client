440d4cf267dd39e3dedc8624c6effad6398037e7
Pavel Khan <pvelkhantopu@gmail.com>
2023-05-18 14:34:17 +0600

    Single toy page done text white to visible

--- src/components/SingleToy/SingleToy.jsx
+++ src/components/SingleToy/SingleToy.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 
 const SingleToy = () => {
@@ -6,14 +7,17 @@ const SingleToy = () => {
     const { name, pictureURL, price, quantity, rating, sellerName, subcategory, _id, description
     } = toy || {};
     return (
-        <div>
-            <div className="card w-full bg-base-100 shadow-lg image-full">
-                <figure><img src={pictureURL} alt="Shoes" /></figure>
+        <div className='my-10'>
+            <div className="card w-9/12 mx-auto h-[500px] shadow-lg image-full">
+                <figure><img className='h-full' src={pictureURL} alt="Picture" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <h2 className="text-4xl font-bold py-5 text-white">{name}</h2>
+                    <div className='space-y-3 text-white'>
+                        <p>Seller Name: {sellerName}</p>
+                        <p>Price: {price}</p>
+                        <p>Available Quantity: {quantity}</p>
+                        <p className='flex items-center gap-2'>Rating: {rating} <span className='text-yellow-500'><FaStar/></span> </p>
+                        <p>Details: {description}</p>
                     </div>
                 </div>
             </div>
