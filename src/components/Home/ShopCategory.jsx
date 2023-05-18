4b1d9e623d16cf4ea536adce16094fe13439a4f1
Pavel Khan <pvelkhantopu@gmail.com>
2023-05-18 13:04:38 +0600

    Shop by category part done

---
+++ src/components/Home/ShopCategory.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import Toys from './Toys';
+
+const ShopCategory = () => {
+    const [toys, setToys] = useState([]);
+    const [activeTab, setActiveTab] = useState("Avengers");
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/toys/${activeTab}`)
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [activeTab])
+
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+      };
+
+    // console.log(toys);
+    return (
+        <div className='text-center'>
+            <h1 className="text-5xl font-bold my-10">Shop By Category</h1>
+                <div className=" w-fit mx-auto">
+                    <div className="tabs d-flex justify-between gap-1 mb-5">
+                        <div
+                            onClick={() => handleTabClick("Avengers")}
+                            className={`px-4 py-2 Avengers ${activeTab == "Avengers" ? " bg-red-700 text-white" : "bg-base-200"
+                                }`}
+                        >
+                            Avengers
+                        </div>
+                        <div
+                            onClick={() => handleTabClick("X-Men")}
+                            className={`px-4 py-2 X-Men ${activeTab == "X-Men" ? " bg-red-700 text-white" : "bg-base-200"
+                                }`}
+                        >
+                            X-Men
+                        </div>
+                        <div
+                            onClick={() => handleTabClick("Ghost Rider")}
+                            className={`px-4 py-2 Ghost Rider ${activeTab == "Ghost Rider" ? " bg-red-700 text-white" : "bg-base-200"
+                                }`}
+                        >
+                            Ghost Rider
+                        </div>
+                    </div>
+                </div>
+            <div className='grid grid-cols-2 w-full justify-between gap-10'>
+                {
+                    toys.map(toy => <Toys key={toy._id} toy={toy}></Toys>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ShopCategory;
---
+++ src/components/Home/Toys.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+const Toys = ({ toy }) => {
+
+    const { name, pictureURL, price, quantity, rating, sellerName, subcategory, _id, description
+    } = toy || {};
+
+    return (
+        <div className="card w-full bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={pictureURL} alt="Shoes" className="rounded-xl h-72 w-9/12" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{name}</h2>
+                <p>Price: {price}</p>
+                <p className='flex items-center gap-2'>Rating: {rating} <span className='text-yellow-500'><FaStar/></span> </p>
+                <div className="card-actions">
+                    <button className="btn btn-secondary text-white">View Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Toys;
