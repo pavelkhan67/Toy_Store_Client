4d0ca33ffcfe5d1da5770130163cb2fd690774ea
Pavel Khan <pvelkhantopu@gmail.com>
2023-05-18 09:00:40 +0600

    Register page done

--- src/components/UserLog/Register.jsx
+++ src/components/UserLog/Register.jsx
@@ -1,29 +1,23 @@
 import React, { useContext } from 'react';
-import img from "../../assets/img/login.jpg"
-import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
+import img from "../../assets/img/login.jpg"
+import { Link } from 'react-router-dom';
 import GoogleLogin from '../Shared/GoogleLogin';
+const Register = () => {
+    const {createUser} = useContext(AuthContext);
 
-const Login = () => {
-    const {signIn} = useContext(AuthContext);
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    const from = location.state?.from?.pathname || '/';
-
-    const handleLogin = event => {
+    const handleSignUp = event => {
         event.preventDefault();
         const form = event.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
+        const photo = form.photo.value;
 
-        signIn(email, password)
+        createUser(email, password)
         .then(result => {
             const user = result.user;
             console.log(user);
-            alert('Login Success')
-            navigate(from, {replace: true})
-            form.reset();
         })
         .catch(error => {
             console.log(error);
@@ -37,9 +31,15 @@ const Login = () => {
                     <img className='rounded-lg' src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm border-2 bg-base-100">
-                    <h1 className="text-3xl font-bold text-center py-5">Login</h1>
-                    <form onSubmit={handleLogin}>
+                    <h1 className="text-3xl font-bold text-center py-5">Register</h1>
+                    <form onSubmit={handleSignUp}>
                         <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -51,22 +51,24 @@ const Login = () => {
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <span className="label-text">Photo URL</span>
                                 </label>
+                                <input type="text" name='photo' placeholder="photo" className="input input-bordered" />
                             </div>
                             <div className="form-control mt-6">
-                                <input className="btn btn-secondary text-white" type="submit" value="Login" />
+                                <input className="btn btn-secondary text-white" type="submit" value="Register" />
                             </div>
                         </div>
                     </form>
-                    <p className='text-center pb-5 text-sm'>New To Toy Mania? <Link className='text-secondary  font-semibold' to="/register">Register</Link></p>
+                    <p className='text-center pb-5 text-sm'>Already have an account? <Link className='text-secondary  font-semibold' to="/login">Login</Link></p>
                     <GoogleLogin></GoogleLogin>
                 </div>
             </div>
-
         </div>
     );
 };
 
-export default Login;
+export default Register;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../components/Home/Home";
 import Login from "../components/UserLog/Login";
+import Register from "../components/UserLog/Register";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
         {
           path: "/login",
           element: <Login></Login>
+        },
+        {
+          path: "/register",
+          element: <Register></Register>
         },
     
       ]
